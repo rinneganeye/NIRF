de71cf3c8e413d29f7b4870c16b7fb5b168b2f19
Amey Shinde <ameyshinde2001@gmail.com>
2023-11-23 19:48:14 +0530

    modified:   app/(forms)/faculties/faculty-details/page.jsx

--- app/(forms)/faculties/faculty-details/page.jsx
+++ app/(forms)/faculties/faculty-details/page.jsx
@@ -1,9 +1,180 @@
+"use client"
 import Layout from "@/app/dashboard/layout";
+import { Button, Typography } from "@material-tailwind/react";
 
 const page = () => {
     return (
         <Layout>
-            Faculty Details
+            <main className="w-full px-5 md:px-20">
+                <Typography className="text-center m-4 text-2xl font-semibold">Faculty Details</Typography>
+                <form action="#" className="flex justify-center flex-col my-8">
+                    <section className="container w-full mb-3 flex flex-col gap-3 sm:flex-row sm:justify-around md:gap-16 md:mb-8 lg:gap-28">
+                        <div className="fields w-full">
+                            <label htmlFor="">Sr.No</label>
+                            <input type="text" name="" className="bg-gray-100 w-full rounded-sm p-1 mt-1 md:p-2" />
+                        </div>
+                        <div className="fields w-full">
+                            <label htmlFor="">Year</label>
+                            <input type="text" name="" className="bg-gray-100 w-full rounded-sm p-1 mt-1 md:p-2" />
+                        </div>
+                    </section>
+                    <section className="container w-full mb-3 flex flex-col gap-3 sm:flex-row sm:justify-around md:gap-16 md:mb-8 lg:gap-28">
+                        <div className="fields w-full">
+                            <label htmlFor="">Department ID </label>
+                            <input type="text" name="" className="bg-gray-100 w-full rounded-sm p-1 mt-1 md:p-2" />
+                        </div>
+                        <div className="fields w-full">
+                            <label htmlFor="">Faculty Name </label>
+                            <input type="text" name="" className="bg-gray-100 w-full rounded-sm p-1 mt-1 md:p-2" />
+                        </div>
+                    </section>
+                    {/* DROPDOWN  */}
+                    <section className="container w-full mb-3 flex flex-col gap-3 sm:flex-row sm:justify-around md:gap-16 md:mb-8 lg:gap-28">
+                        <div className="fields w-full flex flex-col">
+                            <label htmlFor="">Gender</label>
+                            <select type="text" name="" className="bg-gray-100 w-full md:max-w-[45%] rounded-sm p-1 mt-1 md:p-2">
+                                <option value="Select" disabled>Select</option>
+                                <option value="Indian">Male</option>
+                                <option value="Foreigner">Female</option>
+                            </select>
+                        </div>
+                    </section>
+                    <section className="container w-full mb-3 flex flex-col gap-3 sm:flex-row sm:justify-around md:gap-16 md:mb-8 lg:gap-28">
+                        <div className="fields w-full">
+                            <label htmlFor="">Designatiom</label>
+                            <input type="text" name="" className="bg-gray-100 w-full rounded-sm p-1 mt-1 md:p-2" />
+                        </div>
+                        <div className="fields w-full">
+                            <label htmlFor="">Date of Birth</label>
+                            <input type="date" name="" className="bg-gray-100 w-full rounded-sm p-1 mt-1 md:p-2" />
+                        </div>
+                    </section>
+                    <section className="container w-full mb-3 flex flex-col gap-3 sm:flex-row sm:justify-around md:gap-16 md:mb-8 lg:gap-28">
+                        <div className="fields w-full">
+                            <label htmlFor="">Age</label>
+                            <input type="text" name="" className="bg-gray-100 w-full rounded-sm p-1 mt-1 md:p-2" />
+                        </div>
+                        <div className="fields w-full">
+                            <label htmlFor="">Qualification</label>
+                            <input type="text" name="" className="bg-gray-100 w-full rounded-sm p-1 mt-1 md:p-2" />
+                        </div>
+                    </section>
+                    <section className="container w-full mb-3 flex flex-col gap-3 sm:flex-row sm:justify-around md:gap-16 md:mb-8 lg:gap-28">
+                        <div className="fields w-full">
+                            <label htmlFor="">Experience</label>
+                            <input type="text" name="" className="bg-gray-100 w-full rounded-sm p-1 mt-1 md:p-2" />
+                        </div>
+                        <div className="fields w-full">
+                            <label htmlFor="">PAN Number</label>
+                            <input type="text" name="" className="bg-gray-100 w-full rounded-sm p-1 mt-1 md:p-2" />
+                        </div>
+                    </section>
+                    <section className="container w-full mb-3 flex flex-col gap-3 sm:flex-row sm:justify-around md:gap-16 md:mb-8 lg:gap-28">
+                        <div className="fields w-full">
+                            <label htmlFor="">Experience</label>
+                            <input type="text" name="" className="bg-gray-100 w-full rounded-sm p-1 mt-1 md:p-2" />
+                        </div>
+                        <div className="fields w-full">
+                            <label htmlFor="">PAN Number</label>
+                            <input type="text" name="" className="bg-gray-100 w-full rounded-sm p-1 mt-1 md:p-2" />
+                        </div>
+                    </section>
+                    <section className="container w-full mb-3 flex flex-col gap-3 sm:flex-row sm:justify-around md:gap-16 md:mb-8 lg:gap-28">
+                        <div className="fields w-full flex flex-col">
+                            <label htmlFor="">Whether faculty is associated with the
+                                institute in previous academic
+                                year(2022-2023)?
+                            </label>
+                            <select type="text" name="" className="bg-gray-100 w-full md:max-w-[45%] rounded-sm p-1 mt-1 md:p-2">
+                                <option value="Select" disabled>Select</option>
+                                <option value="Indian">Yes</option>
+                                <option value="Foreigner">No</option>
+                            </select>
+                        </div>
+                    </section>
+                    <section className="container w-full mb-3 flex flex-col gap-3 sm:flex-row sm:justify-around md:gap-16 md:mb-8 lg:gap-28">
+                        <div className="fields w-full flex flex-col">
+                            <label htmlFor="">Faculty Experience in the relavent
+                                Subject Area(Teaching- &
+                                Industrial)-31 July,2023
+                            </label>
+                            <select type="text" name="" className="bg-gray-100 w-full md:max-w-[45%] rounded-sm p-1 mt-1 md:p-2">
+                                <option value="Select" disabled>Select</option>
+                                <option value="Indian">Yes</option>
+                                <option value="Foreigner">No</option>
+                            </select>
+                        </div>
+                    </section>
+                    <section className="container w-full mb-3 flex flex-col gap-3 sm:flex-row sm:justify-around md:gap-16 md:mb-8 lg:gap-28">
+                        <div className="fields w-full flex flex-col">
+                            <label htmlFor="">Currently working with institution?
+                            </label>
+                            <select type="text" name="" className="bg-gray-100 w-full md:max-w-[45%] rounded-sm p-1 mt-1 md:p-2">
+                                <option value="Select" disabled>Select</option>
+                                <option value="Indian">Yes</option>
+                                <option value="Foreigner">No</option>
+                            </select>
+                        </div>
+                    </section>
+                    <section className="container w-full mb-3 flex flex-col gap-3 sm:flex-row sm:justify-around md:gap-16 md:mb-8 lg:gap-28">
+                        <div className="fields w-full">
+                            <label htmlFor="">Association Type</label>
+                            <input type="text" name="" className="bg-gray-100 w-full rounded-sm p-1 mt-1 md:p-2" />
+                        </div>
+                        <div className="fields w-full">
+                            <label htmlFor="">Email</label>
+                            <input type="text" name="" className="bg-gray-100 w-full rounded-sm p-1 mt-1 md:p-2" />
+                        </div>
+                    </section>
+                    <section className="container w-full mb-3 flex flex-col gap-3 sm:flex-row sm:justify-around md:gap-16 md:mb-8 lg:gap-28">
+                        <div className="fields w-full">
+                            <label htmlFor="">Mobile</label>
+                            <input type="text" name="" className="bg-gray-100 w-full rounded-sm p-1 mt-1 md:p-2" />
+                        </div>
+                        <div className="fields w-full">
+                            <label htmlFor="">Name of Award</label>
+                            <input type="text" name="" className="bg-gray-100 w-full rounded-sm p-1 mt-1 md:p-2" />
+                        </div>
+                    </section>
+                    <section className="container w-full mb-3 flex flex-col gap-3 sm:flex-row sm:justify-around md:gap-16 md:mb-8 lg:gap-28">
+                        <div className="fields w-full flex flex-col">
+                            <label htmlFor="">Level of Award</label>
+                            <select type="text" name="" className="bg-gray-100 w-full md:max-w-[45%] rounded-sm p-1 mt-1 md:p-2">
+                                <option value="Select" >Select</option>
+                                <option value="Indian">National</option>
+                                <option value="Foreigner">International</option>
+                            </select>
+                        </div>
+                    </section>
+                    <section className="container w-full mb-3 flex flex-col gap-3 sm:flex-row sm:justify-around md:gap-16 md:mb-8 lg:gap-28">
+                        <div className="fields w-full">
+                            <label htmlFor="">Name of Award Agency</label>
+                            <input type="text" name="" className="bg-gray-100 w-full rounded-sm p-1 mt-1 md:p-2" />
+                        </div>
+                        <div className="fields w-full">
+                            <label htmlFor="">Address of Award Agency</label>
+                            <input type="text" name="" className="bg-gray-100 w-full rounded-sm p-1 mt-1 md:p-2" />
+                        </div>
+                    </section>
+                    <section className="container w-full mb-3 flex flex-col gap-3 sm:flex-row sm:justify-around md:gap-16 md:mb-8 lg:gap-28">
+                        <div className="fields w-full">
+                            <label htmlFor="">YEAR OF RECEIVED AWARD</label>
+                            <input type="text" name="" className="bg-gray-100 w-full rounded-sm p-1 mt-1 md:p-2" />
+                        </div>
+                        <div className="fields w-full">
+                            <label htmlFor="">                                EMAIL ID OF AGENCY</label>
+                            <input type="text" name="" className="bg-gray-100 w-full rounded-sm p-1 mt-1 md:p-2" />
+                        </div>
+                    </section>
+                    <section className="container w-full mb-3 flex flex-col gap-3 sm:flex-row sm:justify-around md:gap-16 md:mb-8 lg:gap-28">
+                        <div className="fields w-full flex flex-col">
+                            <label htmlFor="">CONTACT OF AGENCY</label>
+                            <input type="text" name="" className="bg-gray-100 w-full md:max-w-[45%] rounded-sm p-1 mt-1 md:p-2" />
+                        </div>
+                    </section>
+                    <Button className="w-24 mt-4">Submit</Button>
+                </form>
+            </main>
         </Layout>
     );
 }
